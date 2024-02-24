c48fc09abad6f2ad1ccb34b568bb39ae2d491804
Mathias Cabrera <mathiasdesign1@gmail.com>
2024-02-23 22:00:17 -0300

    fourth - model mapping using attributes

--- project_EF/Models/Category.cs
+++ project_EF/Models/Category.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 
 namespace project_ef.Models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId{get;set;}
+
+    [Required]
+    [MaxLength(150)]
     public string? Name {get;set;}
     public string? Description {get;set;}
 
--- project_EF/Models/Task.cs
+++ project_EF/Models/Task.cs
@@ -1,18 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.SignalR.Protocol;
 
 namespace project_ef.Models;
 
 public class Task
 {
+    [Key]
     public Guid TaskId {get;set;}
+
+    [ForeignKey("CategoryId")]
     public Guid CategoryId {get;set;}
-    public string Title {get;set;}
-    public string Description {get;set;}
+
+    [Required]
+    [MaxLength(200)]
+    public string? Title {get;set;}
+    public string? Description {get;set;}
     public Priority PriorityTask {get;set;}
     public DateTime CreationDate {get;set;}
     
     //This property will help us when we need to obtain the Category belonging to a Task
-    public Category Category {get;set;}
+    public Category? Category {get;set;}
+
+    [NotMapped]
+    public string? Summary {get;set;}//This property will have the title and summary of its description. Since we don't want it to be generated in the database but only within C#, we use "NotMapped".
 }
 public enum Priority
 {
